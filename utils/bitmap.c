f0a21e9d86b30273fef6e007e03fd00b213db11a
jimdenberg97 <jimvdb97@gmail.com>
2020-02-27 16:59:30 +0100

    Bitmap fix again

--- utils/bitmap.c
+++ utils/bitmap.c
@@ -6,71 +6,116 @@
 /*   By: jivan-de <jivan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/11 15:23:06 by jivan-de       #+#    #+#                */
-/*   Updated: 2020/02/27 14:32:47 by jivan-de      ########   odam.nl         */
+/*   Updated: 2020/02/27 16:56:46 by jivan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d.h"
+#include <libft.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "cub3d.h"
 
-static int		image_size(int w, int h)
+static unsigned char
+	*bitmap_header(int height, int width, int paddsize)
 {
-	int	per_line;
+	static unsigned char	header[BMP_FHSIZE];
+	int						fsize;
 
-	per_line = w * 3;
-	if (per_line % 4 != 0)
-		per_line += 4 - (per_line % 4);
-	return (per_line * h);
+	fsize = BMP_FHSIZE + BMP_IHSIZE + (BMP_BPP * width + paddsize) * height;
+	header[0] = (unsigned char)('B');
+	header[1] = (unsigned char)('M');
+	header[2] = (unsigned char)(fsize);
+	header[3] = (unsigned char)(fsize >> 8);
+	header[4] = (unsigned char)(fsize >> 16);
+	header[5] = (unsigned char)(fsize >> 24);
+	header[10] = (unsigned char)(BMP_FHSIZE + BMP_IHSIZE);
+	return (header);
 }
 
-static void		write_header(int fd, int w, int h)
+static unsigned char
+	*bitmap_infoheader(int height, int width)
 {
-	int total_size;
+	static unsigned char header[BMP_IHSIZE];
 
-	write(fd, "BM", 2);
-	total_size = 54 + image_size(w, h);
-	write(fd, &total_size, 4);
-	write(fd, "\0\0\0\0\x36\0\0\0\x28\0\0\0", 12);
-	write(fd, &w, 4);
-	write(fd, &h, 4);
-	write(fd, "\x01\0\x18\0\0\0\0\0\0\0\0\0\0\0\0\
-		\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 28);
+	header[0] = (unsigned char)(BMP_IHSIZE);
+	header[4] = (unsigned char)(width);
+	header[5] = (unsigned char)(width >> 8);
+	header[6] = (unsigned char)(width >> 16);
+	header[7] = (unsigned char)(width >> 24);
+	header[8] = (unsigned char)(height);
+	header[9] = (unsigned char)(height >> 8);
+	header[10] = (unsigned char)(height >> 16);
+	header[11] = (unsigned char)(height >> 24);
+	header[12] = (unsigned char)(1);
+	header[14] = (unsigned char)(BMP_BPP * 8);
+	return (header);
 }
 
-static void		write_rgb(int fd, t_config *cfg)
+static int
+	bitmap_save(unsigned char *image, int h, int w, char *fname)
 {
-	int x;
-	int y;
-	int pad;
-	int rgb;
+	unsigned char	padding[3];
+	int				paddsize;
+	int				imgfile;
+	int				i;
 
-	x = 0;
-	y = cfg->height - 1;
-	pad = (4 - (cfg->width * 3) % 4) % 4;
-	while (y >= 0)
+	imgfile = open(fname, O_RDWR | O_CREAT | O_TRUNC, 0755);
+	if (imgfile < 0)
+		return (0);
+	paddsize = (4 - (w * BMP_BPP) % 4) % 4;
+	write(imgfile, bitmap_header(h, w, paddsize), BMP_FHSIZE);
+	write(imgfile, bitmap_infoheader(h, w), BMP_IHSIZE);
+	i = h - 1;
+	while (i >= 0)
 	{
-		while (x < cfg->width)
-		{
-			rgb = get_screen_px(&cfg->images[0], x, y);
-			write(fd, &rgb, 3);
-			x++;
-		}
-		x = 0;
-		write(fd, &x, pad);
-		y--;
+		write(imgfile, image + (BMP_BPP * w * i), BMP_BPP * w);
+		write(imgfile, padding, paddsize);
+		i--;
 	}
+	close(imgfile);
+	return (1);
 }
 
-void			bitmap_export(t_config *cfg, char *fname)
+static void 
+	bitmap_setpixel(t_config *cfg, t_vector pos, int z, unsigned char *image)
 {
-	int	fd;
+	unsigned char	*ptr;
 
+	ptr = image + (int)(BMP_BPP * cfg->width * (int)pos.y) +
+		((int)pos.x * BMP_BPP) + z;
+	if (z == 0)
+		*ptr = color_b((unsigned int)cfg->bmp_color);
+	else if (z == 1)
+		*ptr = color_g((unsigned int)cfg->bmp_color);
+	else if (z == 2)
+		*ptr = color_r((unsigned int)cfg->bmp_color);
+}
+
+void
+	bitmap_export(t_config *cfg, char *fname)
+{
+	unsigned char	*image;
+	t_vector		pos;
+	int				ret;
+
+	pos.y = 0;
 	cfg->save = 0;
-	fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0755);
-	if (fd < 0)
-		log_error(cfg, "Failed to open bitmap file.");
-	write_header(fd, cfg->width, cfg->height);
-	write_rgb(fd, cfg);
-	close(fd);
+	image = ft_calloc(cfg->width * cfg->height, BMP_BPP);
+	mem_protect(cfg, image, "Could not allocate bitmap memory.");
+	while (pos.y < cfg->height)
+	{
+		pos.x = 0;
+		while (pos.x < cfg->width)
+		{
+			cfg->bmp_color = get_screen_px(&cfg->images[0], pos.x, pos.y);
+			bitmap_setpixel(cfg, pos, 2, image);
+			bitmap_setpixel(cfg, pos, 1, image);
+			bitmap_setpixel(cfg, pos, 0, image);
+			pos.x++;
+		}
+		pos.y++;
+	}
+	ret = bitmap_save((unsigned char *)image, cfg->height, cfg->width, fname);
+	if (!ret)
+		log_error(cfg, "Could not save bitmap.");
 }
